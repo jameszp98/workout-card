b36f31a0d67ab269cca231dc2d80fd322fb1d5ed
jameszp98 <15074835679@163.com>
2024-08-25 23:27:33 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -4,82 +4,39 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <div class=" bg-gray-300 flex justify-center text-black">
+    <div class=" w-96">
+      <div class="my-10">
+        <img class="w-full h-60" src="" alt="">
+        <div class="flex justify-evenly my-10">
+          <button class="border-2 border-slate-400 rounded px-3 py-0.5">图片上传</button>
+          <button class="border-2 border-slate-400 rounded px-3 py-0.5">图片下载</button>
+        </div>
+      </div>
+      <div>
+        <div class="my-8 text-black">
+          <span>日期显示：</span> <input type="checkbox">
+        </div>
+        <div class="my-8">
+          <span>时间：</span> <input type="date">
+        </div>  
+        <div class="my-8">
+          <span>运动类型：</span> <input type="date">
+        </div>
+        <div class="my-8">
+          <span>运动时长：</span> <input type="text">
+        </div>
+        <div class="my-8">
+          <span>体重展示：</span> <input type="checkbox">
+        </div>
+        <div class="my-8">
+          <span>体重（kg）：</span> <input type="text">
+        </div>
+        <button class="border-2 border-slate-400 rounded px-3 py-0.5">生成</button>
+      </div>
     </div>
-  </header>
-
-  <RouterView />
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
